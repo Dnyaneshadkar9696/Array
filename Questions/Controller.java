332a2a215a53a4a0c325f63d7c3101b6c6320677
ADKAR DNYANESH RAMDAS <dnyaneshadkar@gmail.com>
2026-03-13 07:48:44 +0530

    Add method call for 2D array in Controller

--- Questions/Controller.java
+++ Questions/Controller.java
@@ -5,6 +5,8 @@ public class Controller {
 	public static void main(String[] args) {
 		
 		QuestionsArray ar = new QuestionsArray();
-		ar.call();
+		ar.call(); // 1 D array
+		ar.calling();  // 2 D array
 	}
 }
+
